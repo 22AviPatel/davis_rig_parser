164a696859e5944471b3a6e8086afe48da270192
22AviPatel <avipnea@gmail.com>
2023-06-30 12:52:06 -0400

    re-pushing changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='davis_rig_parser',
-    version='0.1.2',
+    version='0.1.3',
     url='https://github.com/<your-github-username>/my_module',
     author='Avi Patel, Bradly Stone, Daniel Svedberg',
     author_email='avpnea@gmail.com, dan.ake.svedberg@gmail.com',
