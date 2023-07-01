018a90dd8d47a8455cd9cb04135bc159d8019215
22AviPatel <avipnea@gmail.com>
2023-07-01 18:36:20 -0400

    Bug fixes

--- tests/test.py
+++ tests/test.py
@@ -8,7 +8,6 @@ Created on Wed Jun 28 17:25:29 2023
 
 import davis_rig_parser as drp
 
-
 df = drp.create_df(
     dir_name = '/home/senecascott/Documents/CodeVault/davis_rig_parser/tests/test_data', 
     info_name=None, 
